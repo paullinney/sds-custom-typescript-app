94746384abb09adcabca3951ec847e9735791155
Paul Linney <paul.linney@sds.co.uk>
2019-02-06 21:23:33 +0000

    Initial commit for custom typescript app

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Models
+import Person  from './models/Person';
+// Classes
+import Student  from './Student/Student';
+
+function greeter(person : Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+var user = new Student("Paul", "M.", "Linney");
+
+document.body.innerHTML = greeter(user);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+  mode: 'production'
+};
