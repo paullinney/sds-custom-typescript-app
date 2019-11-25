bcc8d4e7cece3aad1ab538521ea2be04a320cda7
Paul Linney <paul.linney@sds.co.uk>
2019-11-25 19:58:31 +0000

    Added hash to output

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   ],
   mode: 'production',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   }
 };
